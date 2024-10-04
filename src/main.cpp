12806c2d0060111f859fe8b57ff3690440353f1f
Samandar-Komilov <komilov.samandar07@gmail.com>
2024-10-05 00:58:07 +0500

    Added command execution with arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <vector>
 
 #define BUFFSIZE 1024
 
 using namespace std;
+
+
+vector<char*> parseInput(string input){
+    vector<char*> args;
+    char* token = strtok(&input[0], " ");
+    while (token != nullptr){
+        args.push_back(token);
+        token = strtok(nullptr, " ");
+    }
+    args.push_back(nullptr);
+
+    return args;
+}
 
 
 int main(){
@@ -28,13 +43,26 @@ int main(){
         }
 
         // Read command and arguments
-        
+        vector<char*> args = parseInput(input);
 
         // fork() a new process
-
-        // execute command in that child process using execvp
-
-        // wait until that process terminates
+        pid_t pid = fork();
+        if (pid < 0){
+            cerr << "Fork failed!" << endl;
+            continue;
+        }
+        else if (pid == 0){
+            // execute command in that child process using execvp
+            if (execvp(args[0], &args[0]) < 0){
+                cerr << "Command execution failed: No such command or argument exist." << endl;
+            }
+            cout << "DEBUG: " << &args[1] << endl;
+            exit(1);
+        }
+        else {
+            // wait until that process terminates
+            waitpid(pid, nullptr, 0);
+        }
 
         // Restart the job, until user enters `exit` or CTRL+C signal
     }
